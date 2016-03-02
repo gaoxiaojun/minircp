fe20201a768515cd0387f0b76a16c0c766cf939d
Xiaojun Gao <gaoxiaojun@gmail.com>
2016-03-03 03:18:22 +0800

    import mitk

---
+++ Modules/ModuleList.cmake
@@ -0,0 +1,21 @@
+# The entries in the mitk_modules list must be
+# ordered according to their dependencies.
+
+set(mitk_modules
+  #CTKCore
+  #PluginFramework
+  Core
+  CommandLine
+  AppUtil
+  RDF
+  DataTypesExt
+  AlgorithmsExt
+  Qt4Qt5TestModule
+  GraphAlgorithms
+  QmlItems
+  QtWidgets
+  QtWidgetsExt
+  Python
+  QtPython
+  Persistence
+)
